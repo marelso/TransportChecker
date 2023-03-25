aebe28b92d26db8b1a581335d91e1969c9a94608
marelso <klein.marcelojr@gmail.com>
2023-03-24 22:49:34 -0300

    Initial create

---
+++ Main.cs
@@ -0,0 +1,13 @@
+using MaterialSkin.Controls;
+using System.ComponentModel.DataAnnotations;
+
+namespace TransportChecker
+{
+    public partial class Main : MaterialForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
