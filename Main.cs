b8161d94622391ebb89a1f7af2c3fadd882bba72
marelso <klein.marcelojr@gmail.com>
2023-03-24 23:02:03 -0300

    Including material themes.

--- Main.cs
+++ Main.cs
@@ -5,9 +5,24 @@ namespace TransportChecker
 {
     public partial class Main : MaterialForm
     {
+        private readonly MaterialSkin.MaterialSkinManager manager;
+
         public Main()
         {
             InitializeComponent();
+
+            manager = MaterialSkin.MaterialSkinManager.Instance;
+            manager.EnforceBackcolorOnAllComponents = true;
+            manager.AddFormToManage(this);
+
+            manager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            manager.ColorScheme = new MaterialSkin.ColorScheme(
+                MaterialSkin.Primary.LightGreen500
+                , MaterialSkin.Primary.LightGreen700
+                , MaterialSkin.Primary.LightBlue200
+                , MaterialSkin.Accent.DeepOrange200
+                , MaterialSkin.TextShade.WHITE
+                );
         }
     }
 }
